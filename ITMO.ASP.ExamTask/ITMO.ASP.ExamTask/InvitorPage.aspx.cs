4eb91763995bdaf4fa154708704f5232d58f2897
Константин Филюрин <rfrsk@yandex.ru>
2021-04-26 01:11:27 +0300

    Начата логика программы
    Начата логика

---
+++ ITMO.ASP.ExamTask/ITMO.ASP.ExamTask/InvitorPage.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Configuration;
+
+namespace ITMO.ADO.ExamTask
+    {
+    public partial class _Default : Page
+        {
+
+        public int PeopleQuantityProp { get; set; }
+        public string DescriptionProp { get; set; }
+
+        public DateTime StartDateProp { get; set; }
+        public DateTime EndDateProp { get; set; }
+        public string TagsProp { get; set; }
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        //Работа с интерфейсом
+        #region InterfaceWork
+        protected void TextBoxPeopleQuantity_TextChanged(object sender, EventArgs e)
+            {
+                PeopleQuantityProp = Convert.ToInt32(TextBoxPeopleQuantity.Text);           
+            }
+        protected void CalendarStart_SelectionChanged(object sender, EventArgs e)
+            {
+            StartDateProp = Convert.ToDateTime(CalendarStart.SelectedDate);
+            }
+
+        protected void CalendarEnd_SelectionChanged(object sender, EventArgs e)
+            {
+            EndDateProp = Convert.ToDateTime(CalendarEnd.SelectedDate);
+            }
+        protected void ButtonCreateEvent_Click(object sender, EventArgs e)
+            {
+            var context = new DataClassesDataContext(ConfigurationManager.ConnectionStrings["CheaperTogetherConnectionString"].ConnectionString);
+            var newEvent = new Events { PeopleQuantity = PeopleQuantityProp, Description=DescriptionProp, EventDateStart= StartDateProp, EventDateEnd= EndDateProp};
+            context.Events.InsertOnSubmit(newEvent);
+            context.Events.Context.SubmitChanges();           
+            }
+
+
+        #endregion InterfaceWork
+        }
+}
---
+++ ITMO.ASP.ExamTask/ITMO.ASP.ExamTask/InvitorPage.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace ITMO.ADO.ExamTask
+    {
+
+
+    public partial class _Default
+        {
+
+        /// <summary>
+        /// SqlDataSource элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource;
+
+        /// <summary>
+        /// LabelPeopleQuantity элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelPeopleQuantity;
+
+        /// <summary>
+        /// TextBoxPeopleQuantity элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxPeopleQuantity;
+
+        /// <summary>
+        /// LabelDescription элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelDescription;
+
+        /// <summary>
+        /// TextBoxDescription элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxDescription;
+
+        /// <summary>
+        /// LabelPhoto элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelPhoto;
+
+        /// <summary>
+        /// LabelDateStart элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelDateStart;
+
+        /// <summary>
+        /// CalendarStart элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar CalendarStart;
+
+        /// <summary>
+        /// LabelTags элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelTags;
+
+        /// <summary>
+        /// TextBoxTags элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxTags;
+
+        /// <summary>
+        /// LabelDateEnd элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelDateEnd;
+
+        /// <summary>
+        /// CalendarEnd элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar CalendarEnd;
+
+        /// <summary>
+        /// ButtonCreateEvent элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonCreateEvent;
+
+        /// <summary>
+        /// GridViewEvents элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridViewEvents;
+        }
+    }
